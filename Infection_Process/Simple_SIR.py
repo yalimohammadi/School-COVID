71a8bd509e8f0759ca03bf8b92048c4f4063abad
yeganeh <yeganehalimohammadi@gmail.com>
2020-10-13 23:44:08 -0700

    simple random test and random 2

--- Infection_Process/Simple_SIR.py
+++ Infection_Process/Simple_SIR.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from Graph_Generator.single_school_generator import School
 import numpy as np
 
-from Testing_Strategies.Simple_Random import fully_random_test
+from Testing_Strategies import Simple_Random
 
 def plot_simple_SIR(t,S,I,R,alpha=.1,last=True):
 
@@ -22,11 +22,11 @@ def plot_simple_SIR(t,S,I,R,alpha=.1,last=True):
 def SIR_on_weighted_Graph(G,removal_rate = 1.,transmission_scale=1.,initial_fraction_infected= 0.01,num_sim=1):
     for i in range(num_sim-1):
         t,S,I,T,R=EoN.fast_SIR(G,gamma=removal_rate, tau=transmission_scale,transmission_weight="weight",
-                               rho=initial_fraction_infected, all_test_times=np.linspace(0,2,10),test_args=(100),test_func=fully_random_test)
+                               rho=initial_fraction_infected, all_test_times=np.linspace(0,2,10),test_args=(100,),test_func=Simple_Random.fully_random_test)
         plot_simple_SIR(t, S, I, R,last=False)
 
     t, S, I, T, R = EoN.fast_SIR(G, gamma=removal_rate, tau=transmission_scale, transmission_weight="weight",
-                              rho=initial_fraction_infected,all_test_times=np.linspace(0,2,10),test_args=(100),test_func=fully_random_test)
+                              rho=initial_fraction_infected,all_test_times=np.linspace(0,2,10),test_args=(100,),test_func=Simple_Random.fully_random_test)
     plot_simple_SIR(t, S, I, R)
     return t,S,I,T,R
 
