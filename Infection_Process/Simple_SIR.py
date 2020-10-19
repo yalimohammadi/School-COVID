fbdc2a681e1111e6e94ad67e58fdb3eb695d9463
shiragur <shiragur@stanford.edu>
2020-10-18 18:52:43 -0700

    uncommented and reverted back to old parameter setting

--- Infection_Process/Simple_SIR.py
+++ Infection_Process/Simple_SIR.py
@@ -36,8 +36,8 @@ def SIR_on_weighted_Graph(G,removal_rate = 1.,transmission_scale=1.,initial_frac
     for i in range(num_sim):
         t,S,E,I,T,R,status=EoN.fast_SIR(G,gamma=removal_rate, tau=transmission_scale,transmission_weight="weight",
                                rho=initial_fraction_infected, all_test_times = np.linspace(0,119,120),test_args=(100,),test_func=Simple_Random.fully_random_test,weighted_test=False)
-        print_infected_teachers(status)
-        #plot_simple_SIR(t, S, E, I, T, R,last=False)
+        #print_infected_teachers(status)
+        plot_simple_SIR(t, S, E, I, T, R,last=False)
         #print("I= ",I)
         #print("T= ",T)
         print("Full Random strategy: Total number of infected= ", R[len(R)-1])
@@ -50,11 +50,11 @@ def SIR_on_weighted_Graph(G,removal_rate = 1.,transmission_scale=1.,initial_frac
         plot_simple_SIR(t, S, E, I, T, R)
         print("Within Cohort Random strategy: Total number of infected= ", R[len(R) - 1])
         #
-        #t, S, E, I, T, R = EoN.fast_SIR(G, gamma=removal_rate, tau=transmission_scale, transmission_weight="weight",
-        #                             rho=initial_fraction_infected, all_test_times=np.linspace(0, 119, 120),
-        #                             test_args=(school, 100,),weighted_test=True)
+        t, S, E, I, T, R = EoN.fast_SIR(G, gamma=removal_rate, tau=transmission_scale, transmission_weight="weight",
+                                     rho=initial_fraction_infected, all_test_times=np.linspace(0, 119, 120),
+                                     test_args=(school, 100,),weighted_test=True)
         #plot_simple_SIR(t, S, E, I, T, R)
-        #print("Weighted testing strategy: Total number of infected= ", R[len(R) - 1])
+        print("Weighted testing strategy: Total number of infected= ", R[len(R) - 1])
 
     return t,S,E,I,T,R
 
@@ -65,11 +65,11 @@ total_students=2000
 num_grades=4
 num_teachers=60
 num_of_students_within_grade=int(total_students/num_grades)
-p_c=0.1 # [0.05,0.1,,0.2,0.4]
-cg_scale=10 #5 # [5,10]
+p_c=0.05 # [0.05,0.1,,0.2,0.4]
+cg_scale=1/10 #5 # [5,10]
 p_g=p_c*cg_scale
 alpha=0.5
-high_infection_rate=low_infection_rate=(5/7)*alpha
+high_infection_rate=low_infection_rate=(1/7)*alpha
 scale=1/5
 intra_cohort_infection_rate=high_infection_rate*scale
 #print(intra_cohort_infection_rate)
@@ -81,7 +81,7 @@ infection_rate_between_teachers=high_infection_rate*scale #teachers are similar 
 high_risk_probability=0 #(fixed, irrelevant for now)
 initial_fraction_infected= 0.001 #initial fraction infected (fix this)
 transmission_scale= 1 #transmission rate per edge (fix this)
-removal_rate = 1/12 #recovery rate per node (fix this)
+removal_rate = 1/10#recovery rate per node (fix this)
 
 #number of days to recover =  time to recovery for non-hospitalized cases (mean: 13.1 days, 95% CI: 8.3, 16.9)
 #num_days_between_exposed_infection=Weibull distribution with mean 5.4 days (95% CI: 2.4, 8.3)
