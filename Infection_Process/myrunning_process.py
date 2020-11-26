6b88768286e606f4eb14bb3a75c820de1bc87b08
yeganeh <yeganehalimohammadi@gmail.com>
2020-11-26 10:29:38 -0800

    assign teacher to cohort

--- Infection_Process/myrunning_process.py
+++ Infection_Process/myrunning_process.py
@@ -26,6 +26,19 @@ def SIR_on_weighted_Graph(G,school,number_of_tests=0,fraction_infected_at_each_t
     total_infected120_list = []
     total_infected150_list = []
 
+    total_positives30_list = []
+    total_positives60_list = []
+    total_positives90_list = []
+    total_positives120_list = []
+    total_positives150_list = []
+
+    FN30_list = []
+    FN60_list = []
+    FN90_list = []
+    FN120_list = []
+    FN150_list = []
+
+
     # total_active_infected30_list = []
     # total_active_infected60_list = []
     # total_active_infected90_list = []
@@ -38,50 +51,85 @@ def SIR_on_weighted_Graph(G,school,number_of_tests=0,fraction_infected_at_each_t
                                                  weighted_test=False,school=school,isolate=True,tmax=tmax)
         final_infected_FR.append(R[-1]+I[-1]) # Since the process has not ended, we need to add I[-1]
         k=0
-        total_infected30=0
-        total_infected60=0
-        total_infected90=0
-        total_infected120=0
-        total_infected150=0
+        max_infected30=0
+        max_infected60=0
+        max_infected90=0
+        max_infected120=0
+        max_infected150=0
+
+        total_positives30 = 0
+        total_positives60 = 0
+        total_positives90 = 0
+        total_positives120 = 0
+        total_positives150 = 0
+
+        max_false_negative30 = 0
+        max_false_negative60 = 0
+        max_false_negative90 = 0
+        max_false_negative120 = 0
+        max_false_negative150 = 0
 
         for k in range(len(t)):
             if t[k]<=30:
-                total_infected30=max(I[k],total_infected30)
+                max_infected30=max(I[k],max_infected30)
+                total_positives30 = T[k] #it will save the last value of T before 30 days
+                max_false_negative30 = max(E[k]/(I[k]+E[k]),max_false_negative30)
 
             if t[k]<=60:
-                total_infected60=max(I[k],total_infected60)
+                max_infected60=max(I[k],max_infected60)
+                total_positives60 = T[k]
+                max_false_negative60 = max(E[k]/(I[k]+E[k]),max_false_negative60)
 
             if t[k]<=90:
-                total_infected90=max(I[k],total_infected90)
+                max_infected90=max(I[k],max_infected90)
+                total_positives90 = T[k]
+                max_false_negative90 = max(E[k]/(I[k]+E[k]),max_false_negative90)
 
             if t[k]<=120:
-                total_infected120=max(I[k],total_infected120)
+                max_infected120=max(I[k],max_infected120)
+                total_positives120 = T[k]
+                max_false_negative120 = max(E[k]/(I[k]+E[k]),max_false_negative120)
 
             if t[k]<=150:
-                total_infected150=max(I[k],total_infected150)
+                max_infected150=max(I[k],max_infected150)
+                total_positives150 = T[k]
+                max_false_negative150 = max(E[k]/(I[k]+E[k]),max_false_negative150)
 
-        total_infected30_list.append(total_infected30)
-        total_infected60_list.append(total_infected60)
-        total_infected90_list.append(total_infected90)
-        total_infected120_list.append(total_infected120)
-        total_infected150_list.append(total_infected150)
+        total_infected30_list.append(max_infected30)
+        total_infected60_list.append(max_infected60)
+        total_infected90_list.append(max_infected90)
+        total_infected120_list.append(max_infected120)
+        total_infected150_list.append(max_infected150)
 
-        if total_infected30>outbreak:
+        FN30_list.append(max_false_negative30)
+        FN60_list.append(max_false_negative60)
+        FN90_list.append(max_false_negative90)
+        FN120_list.append(max_false_negative120)
+        FN150_list.append(max_false_negative150)
+
+        total_positives30_list.append(total_positives30)
+        total_positives60_list.append(total_positives60)
+        total_positives90_list.append(total_positives90)
+        total_positives120_list.append(total_positives120)
+        total_positives150_list.append(total_positives150)
+
+        if max_infected30>outbreak:
             num_outbreak_FR30+=1
 
-        if total_infected60>outbreak:
+        if max_infected60>outbreak:
             num_outbreak_FR60+=1
 
-        if total_infected90>outbreak:
+        if max_infected90>outbreak:
             num_outbreak_FR90+=1
 
-        if total_infected120>outbreak:
+        if max_infected120>outbreak:
             num_outbreak_FR120+=1
 
-        if total_infected150>outbreak:
+        if max_infected150>outbreak:
             num_outbreak_FR150+=1
 
-    return num_outbreak_FR30/num_sim,num_outbreak_FR60/num_sim,num_outbreak_FR90/num_sim,num_outbreak_FR120/num_sim,num_outbreak_FR150/num_sim, total_infected30_list, total_infected60_list, total_infected90_list, total_infected120_list, total_infected150_list #num_outbreak_RWC/num_sim #
+    return num_outbreak_FR30/num_sim,num_outbreak_FR60/num_sim,num_outbreak_FR90/num_sim,num_outbreak_FR120/num_sim,num_outbreak_FR150/num_sim, total_infected30_list, total_infected60_list, total_infected90_list, total_infected120_list, total_infected150_list,\
+           # total_positives30_list,  total_positives60_list,  total_positives90_list,  total_positives120_list,  total_positives150_list, FN30_list, FN60_list, FN90_list, FN120_list, FN150_list
 
 
 
@@ -131,7 +179,7 @@ interaction_list=[1]
 
 
 #Edge weights
-high_infection_rate = 2.86/100.
+high_infection_rate = 2.2/100.
 low_infection_rate= high_infection_rate #Within cohort edge weight
 # scale=1/20
 # intra_cohort_infection_rate = low_infection_rate*scale   #Across cohort edge weights
@@ -179,8 +227,15 @@ outbreak60 = "60 days"
 outbreak90 = "90 days"
 outbreak120 = "120 days"
 outbreak150 = "150 days"
+
+FN_str = "False Negative Rates"
+Total_Infected_str = "maximum number infected"
+test_str= "positive tests"
+
 data_infected = {test_str: [], p_str: [], ICI_str: [], inboud_str: [], outbreak30: [], outbreak60: [], outbreak90: [], outbreak120: [], outbreak150: []}
-full_data_infected = {test_str: [], p_str: [], ICI_str: [], inboud_str: [], outbreak30: [], outbreak60: [], outbreak90: [], outbreak120: [], outbreak150: []}
+full_data_infected = {test_str: [], p_str: [], ICI_str: [], inboud_str: [], outbreak30: [], outbreak60: [], outbreak90: [], outbreak120: [], outbreak150: [],
+
+                      }
 
 diagonal =0
 for testing_fraction in testing_fraction_list:
@@ -226,11 +281,11 @@ for testing_fraction in testing_fraction_list:
     full_data_to_dump = pd.DataFrame(full_data_infected)
     print(data_to_dump)
     print(full_data_to_dump)
-    with open('Highoutput' + str(int(testing_fraction*100)) + 't.data', 'wb') as filehandle:
+    with open('Lowoutput' + str(int(testing_fraction*100)) + 't.data', 'wb') as filehandle:
         # store the data as binary data stream
         pickle.dump(data_to_dump, filehandle)
 
-    with open('HighFulloutput' + str(int(testing_fraction*100)) + 't.data', 'wb') as filehandle:
+    with open('LowFulloutput' + str(int(testing_fraction*100)) + 't.data', 'wb') as filehandle:
         # store the data as binary data stream
         pickle.dump(full_data_to_dump, filehandle)
 
